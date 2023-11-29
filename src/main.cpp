8a69faabb1aec5330d1e39a4bef33acfe3dde645
Kynemallv <matveygor41@gmail.com>
2023-11-29 22:08:20 +0300

    add main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <game_logic/game_logic.h>
+#include <iostream>
+
+int main ()
+{
+  minesweeper::game_logic::Settings settings{2, 2, 1};
+  minesweeper::game_logic::Field field{settings};
+
+  field.mark_cell(0, 0);
+  field.open_cell(0, 0);
+
+  minesweeper::game_logic::field_matrix_t field_v = field.get_field();
+  for (auto row : field_v)
+  {
+    for (auto i : row)
+      std::cout << "is_marked: " << i.is_marked << " is_open: " << i.is_open
+                << " ";
+
+    std::cout << std::endl;
+  }
+}
