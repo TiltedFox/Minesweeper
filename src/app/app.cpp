75132d59d6e5dcc9e678ad6838777630b30e8eb6
Gachick <maksdayt@gmail.com>
2023-12-05 14:59:09 +0300

    Move app states to separate files

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -1,4 +1,5 @@
 #include <app/app.h>
+#include <app/app_states.h>
 
 namespace minesweeper::app {
 void App::set_state(AppState &new_state) {
@@ -12,14 +13,5 @@ App::App(int w, int h, const std::string &title)
   current_state = &TestState1::get_instance();
   current_state->enter(this);
 }
-
-TestState2::TestState2()
-    : test_button{Graph_lib::Point{100, 100}, 50, 50, "Change to state1",
-                  [](Graph_lib::Address, Graph_lib::Address button_addr) {
-                    auto *button =
-                        static_cast<Graph_lib::Button *>(button_addr);
-                    static_cast<App &>(button->window())
-                        .set_state(TestState1::get_instance());
-                  }} {};
 
 } // namespace minesweeper::app
--- src/app/app.h
+++ src/app/app.h
@@ -1,13 +1,11 @@
 #ifndef APP_H
 #define APP_H
 
-#include <memory>
 #include <string>
 
 #include <Graph_lib/GUI.h>
 #include <Graph_lib/Window.h>
 
-#include <app/app_states.h>
 #include <game_logic/game_logic.h>
 
 #include <iostream>
@@ -27,55 +25,15 @@ public:
   App(int w, int h, const std::string &title);
 
   void set_state(AppState &new_state);
-  void test() { std::cout << "Successful callback! "; }
 
-  // private:
+private:
   AppState *current_state;
 };
 
-class TestState1;
-class TestState2;
-
-class TestState2 : public AppState {
-public:
-  static AppState &get_instance() {
-    static TestState2 singleton;
-    return singleton;
-  }
-
-  void enter(App *app) override {
-    app->attach(test_button);
-    std::cout << "Enter state2\n";
-  }
-  void exit(App *app) override { app->detach(test_button); }
-
-private:
-  TestState2();
-  Graph_lib::Button test_button;
-};
-
-class TestState1 : public AppState {
-public:
-  static AppState &get_instance() {
-    static TestState1 singleton;
-    return singleton;
-  }
-
-  void enter(App *app) override {
-    app->attach(test_button);
-    std::cout << "Enter state1\n";
-  }
-  void exit(App *app) override { app->detach(test_button); }
-
-private:
-  Graph_lib::Button test_button{
-      Graph_lib::Point{20, 20}, 50, 50, "Change to state2",
-      [](Graph_lib::Address, Graph_lib::Address button_addr) {
-        auto *button = static_cast<Graph_lib::Button *>(button_addr);
-        static_cast<App &>(button->window())
-            .set_state(TestState2::get_instance());
-      }};
-};
+inline App &get_app_ref(Graph_lib::Address widget_address) {
+  auto *widget_p = static_cast<Graph_lib::Widget *>(widget_address);
+  return static_cast<App &>(widget_p->window());
+}
 
 } // namespace minesweeper::app
 #endif // #ifndef GAME_H
